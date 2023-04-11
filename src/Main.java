7f7826a56d9d1b81bb98d860a5075db5f53d72e5
gk <ishkredov2@gmail.com>
2023-04-11 20:35:56 +0300

    Fixed working thing

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,26 @@
+import classwork.WorkWithUser;
+
 public class Main {
-}
+    public  static void main(String[] args) {
+
+//        WorkWithUser User1 = new WorkWithUser();
+//        User1.age = 55;
+//        User1.gender = "Male";
+//        User1.name = "Alex";
+//        User1.secondName = "Sicorski";
+        WorkWithUser User2 = new WorkWithUser ("Ivan","Shkredov","Male", 20);
+        String UserInfo = User2.getInfoAboutUser();
+        System.out.println(UserInfo);
+// а почему 2 раза выполняет? то что сверху
+//        System.out.println("Check of print" + User2.name);
+        WorkWithUser User3 = new WorkWithUser(56,"Nayda");
+        WorkWithUser user4 = new WorkWithUser("Ivan", "NUMNJ", "SDJKSD", 777);
+        WorkWithUser User5 = new WorkWithUser(66,"UUU");
+        String UserInfo5 = User5.getInfoAboutUser();
+        System.out.println(UserInfo5);
+
+
+
+    }}
+
+
