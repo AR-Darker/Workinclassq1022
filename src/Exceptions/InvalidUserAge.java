895db22024df15ea192f3e21153d0eca0b7d945e
gk <ishkredov2@gmail.com>
2023-05-12 21:14:05 +0300

    Fixed working thing

---
+++ src/Exceptions/InvalidUserAge.java
@@ -0,0 +1,7 @@
+package Exceptions;
+
+public class InvalidUserAge extends RuntimeException {
+    public InvalidUserAge(User user) {
+        super("User has invalid age" = user.getAge());
+    }
+}
---
+++ src/Exceptions/MyUncheckedException.java
@@ -0,0 +1,12 @@
+package Exceptions;
+
+public class MyUncheckedException extends RuntimeException {
+
+    public MyUncheckedException (String message){
+        super(message);
+
+        public MyUncheckedException(User user){
+            super
+        }
+    }
+}
